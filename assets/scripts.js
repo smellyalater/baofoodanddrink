243c98ba6c82e3c66f533b66063a56d8b5a55995
Andy Copleman <acopleman@gmail.com>
2016-11-19 22:50:29 -0500

    initial upload

---
+++ assets/scripts.js
@@ -0,0 +1,46 @@
+
+
+/*
+var client = ShopifyBuy.buildClient({
+    apiKey: 'de64b8923163c5037c6ad39507715662',
+    domain: 'drinkbao.myshopify.com',
+    appId: '6'
+});
+
+var ui = ShopifyBuy.UI.init(client);
+
+
+
+
+$('document').ready(function() {
+
+
+    ui.createComponent('product', {
+    	id: 98350256,
+        node: document.getElementById('test-product'),
+        options: {
+        	product: {
+        		iframe: true,
+        		buttonDesination: 'modal',
+        		order: [
+	        		'title',
+	        		'img',
+	        		'price',
+	        		'button'
+	        	], 
+	        	isButton: true
+        	},
+        window: {
+        	height: 900,
+        	width: 1300
+        }
+        }
+    });
+
+
+
+
+
+
+});
+*/
