9cc2fd8c1ddae77dce1192ecee65e5af266a79e4
Andy Copleman <acopleman@gmail.com>
2016-11-28 19:44:31 -0500

    something

--- assets/scripts.js
+++ assets/scripts.js
@@ -9,6 +9,7 @@
 
 // var ui = ShopifyBuy.UI.init(client);
 
+var aboutSectionChapterHeight;
 
 
 
@@ -34,6 +35,24 @@ $(window).scroll(function(){
 });
 
 
+//define height of cover
+
+//get the height of the chapter contents.
+var aboutSectionChapterHeight = $('.about-section .chapter-contents').height();
+console.log('about section chapter height is ' + aboutSectionChapterHeight);
+
+//fix cover to top of viewport
+
+// keep 100vh and top 60px until height of contents is met.
+
+// $(window).scroll(function() {
+//     if ($(window).scrollTop > aboutSectionChapterHeight) {
+//         console.log('winow scrolltop is greater than about section chapter height');
+//     }
+
+// });
+
+// then keep 100vh but top becomes height of contents AND top of next cover becomes 60px.
 
 /*
 on page load, find out how far nav is from top.
